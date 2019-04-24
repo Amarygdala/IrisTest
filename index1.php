df235b75775df49cdc621ee81f2457a7023025b6
Talha Shaikh <33578360+TalhaShakih2002@users.noreply.github.com>
2019-04-24 08:30:47 -0400

    New folder

--- index1.php
+++ index1.php
@@ -2,27 +2,68 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<title>Iris Test Form</title>
 		<link rel="stylesheet" href="style.css">
+        
+    </head>
 	<body>
+
+
+<div class="burger">
+    <button onclick="dropFunction()" class="dropbtn"></button>
+    <div id="dropMenu" class="menuContent">
+        <a href="index1.php">Form</a>
+        <a href="showTable.php">Show Table</a>
+        <a href="deleteRecord.php">Delete Record</a>
+    </div>
+</div>
+
+<div class="burger"></div>
+<div class="burger"></div>
+
+<script type="text/javascript">
+    function dropFunction(){
+        document.getElementById("dropMenu").classList.toggle("show");
+    }
+    window.onclick = function(event){
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("menuContent");
+            var i;
+            for(i=0;i<dropdowns.length;i++){
+                var openDropdown=dropdowns[i];
+                if(openDropdown.classList.contains('show')){
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+</script>
+
 <form action="action.php" method="POST">
-   <div class="login-box">
-   	<h1>Iris Test Form</h1>
-    <input type= "number" step="any" name="sepal_lengp" placeholder="Sepal Length">
-    <input type= "number" step="any" name="sepal_widthp" placeholder="Sepal Width">
-    <input type= "number" step="any" name="petal_lengp" placeholder="Petal Length">
-    <input type= "number" step="any" name="petal_widthp" placeholder="Petal Width">
+
+   	<h1>Iris Test Form</h1> 
+<div class="formclass">
+    <label for="sepal_lengp">Sepal Length:</label>
+    <input type= "number" step="any" name="sepal_lengp">
+    <label for="sepal_widthp">Sepal Width:</label>
+    <input type= "number" step="any" name="sepal_widthp">
+    <label for="petal_lengp">Petal Length:</label>
+    <input type= "number" step="any" name="petal_lengp">
+    <label for="petal_widthp">Petal Width:</label>
+    <input type= "number" step="any" name="petal_widthp">
+    <label for="Flower_type">Flower Type:</label>
     <select name="Flower_type">
-    	<option value="" disabled selected>Flower type</option>
     	<option value="Iris-setosa">Iris-setosa</option>
     	<option value="Iris-versicolor">Iris-versicolor</option>
     	<option value="Iris-virginica">Iris-virginica</option>
-    </div>
+
  </select>
-    <input type="submit">
+    <button type="submit">Submit</button>
+
 </form>
-<form action="deleteRecord.php" method="POST">
-	<button type="submit">Delete Records</button>
-</form>
+
+</div>
 </body>
+<img src="sap.png">
 </html>
--- showTable.php
+++ showTable.php
@@ -1,6 +1,9 @@
 <html>
 <header>
 <link rel="stylesheet" href="styletable.css">
+
+</header>
+<body>
 <div class="burgerTOP">
 <div class="burger">
     <button onclick="dropFunction()" class="dropbtn"></button>
@@ -10,20 +13,10 @@
         <a href="deleteRecord.php">Delete Record</a>
     </div>
 </div>
+
 <div class="burger"></div>
 <div class="burger"></div>
 </div>
-<div class="formTOP">
-
-<form action="" method="POST">
-    <input type= "number" name="ID" placeholder="ID">
-    <button type="submit">Delete</button>
-</form>
-</div>
-</header>
-<body>
-
-
 <script type="text/javascript">
     function dropFunction(){
         document.getElementById("dropMenu").classList.toggle("show");
@@ -41,31 +34,26 @@
         }
     }
 </script>
-
+</div>
 <?php
 
-	include("C:\Users\Dshop\Desktop\maybexampp\htdocs\Iristestproject\connection.php");
+  include("C:\Users\Dshop\Desktop\maybexampp\htdocs\Iristestproject\connection.php");
     $sql = "SELECT * FROM iris;";
     $sql1="SELECT * FROM iris WHERE Sepal_length = 5";
     $result = mysqli_query( $connection, $sql );
 echo "<table id='datatable'>";
     $resultCheck = mysqli_num_rows($result);
-	/*if(isset($_POST['ID'])){
-		header("Location:/Iristestproject/index1.php");
-	} //move back to form once deletion completes*/
+
     if($resultCheck>0){
-	    	echo "  <tr><th>ID</th><th>Sepal Length</th><th>Sepal Width</th><th>Petal Length</th><th>Petal Width</th><th>Flower Type</th></tr>";
-	    while($all =mysqli_fetch_assoc($result) ){
-	        echo "<tr><td>" . $all['id'] . "</td><td>" . $all['Sepal_length'] . "</td><td>". $all['Sepal_width'] . "</td><td>". $all['Petal_length'] . "</td><td>". $all['Petal_width'] . "</td><td>". $all['Flower_type'] . "</td></tr>"; 
+        echo "  <tr><th>ID</th><th>Sepal Length</th><th>Sepal Width</th><th>Petal Length</th><th>Petal Width</th><th>Flower Type</th></tr>";
+      while($all =mysqli_fetch_assoc($result) ){
+          echo "<tr><td>" . $all['id'] . "</td><td>" . $all['Sepal_length'] . "</td><td>". $all['Sepal_width'] . "</td><td>". $all['Petal_length'] . "</td><td>". $all['Petal_width'] . "</td><td>". $all['Flower_type'] . "</td></tr>"; 
 }
 
 echo "</table>"; //Close the table in HTML
 
-	}
-
-	$deleteID=$_POST['ID']??'';
-	$dbDelete = "DELETE FROM `iris` WHERE `iris`.`id` = $deleteID";
-	mysqli_query( $connection, $dbDelete); 
-
-
+echo "<form action='index1.php'><button type='submit'>Back To Form</button></form>";
+  }
 ?>
+</body>
+</html>
