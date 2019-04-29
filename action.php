b0535eae310f694fe148e1c17cc4c224e5fd7d11
Talha Shaikh <33578360+TalhaShakih2002@users.noreply.github.com>
2019-04-29 08:55:27 -0400

    Update action.php

--- action.php
+++ action.php
@@ -12,20 +12,4 @@
 	$dbinsert = "INSERT INTO iris (Sepal_length, Sepal_width, Petal_length, Petal_width, Flower_type) VALUES ('$sepal_lengp', '$sepal_widthp', '$petal_lengp', '$petal_widthp', '$flower_typep');";
 
 	mysqli_query( $connection, $dbinsert ); 
-
-<?php
-
-	include("C:\Users\Dshop\Desktop\maybexampp\htdocs\Iristestproject\connection.php");
-
-	$sepal_lengp = $_POST["sepal_lengp"]??'';
-	$sepal_widthp = $_POST["sepal_widthp"]??'';
-	$petal_lengp = $_POST["petal_lengp"]??'';
-	$petal_widthp = $_POST["petal_widthp"]??'';
-	$flower_typep = $_POST["Flower_type"]??'';
-
-
-	$dbinsert = "INSERT INTO iris (Sepal_length, Sepal_width, Petal_length, Petal_width, Flower_type) VALUES ('$sepal_lengp', '$sepal_widthp', '$petal_lengp', '$petal_widthp', '$flower_typep');";
-
-	mysqli_query( $connection, $dbinsert ); 
-
-	header("Location: ../Iristestproject/index1.php?=success");
+	
