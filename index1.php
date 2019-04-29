65b3e10cef76a7ac9b0d6ea81249c3ad69825770
Talha Shaikh <33578360+TalhaShakih2002@users.noreply.github.com>
2019-04-29 09:12:00 -0400

    unintended scrolling
    parralax scrolling

--- index1.php
+++ index1.php
@@ -26,9 +26,6 @@
 <script>
 function parallax(){
 	var prlx_lyr_1 = document.getElementById('prlx_lyr_1');
-	var prlx_lyr_2 = document.getElementById('prlx_lyr_2');
-	prlx_lyr_1.style.top = -(window.pageYOffset / 4)+'px';
-	prlx_lyr_2.style.top = -(window.pageYOffset / 8)+'px';
 }
 window.addEventListener("scroll", parallax, false);
 </script>
